45871f63a0214151ba34f9e0d2ba5fd5f931944a
pearlrhema <pearlrhema8@gmail.com>
2024-07-10 19:26:11 +0100

    filtered temples

--- scripts/filtered-temple.js
+++ scripts/filtered-temple.js
@@ -135,30 +135,106 @@ const temples = [
     imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   }
 ];
-const smallTemples = document.getElementById("small temples");
-smallTemples.addEventListener("click", function(temples) {
-  temples.forEach(temple => {
-    let card = document.createElement("section");
-    let name = document.createElement("h3");
-    let location = document.createElement("h3");
-    let dedication = document.createElement("h3");
-    let area = document.createElement("h3");
-    let img = document.createElement("img");
 
-    name.textContent = temple.templeName;
-    location.innerHTML = `<span class="infoLabel">Location:</span> ${temple.location}`;
-    dedication.innerHTML = `<span>Dedicated:</span> ${temple.dedicated}`;
-    area.innerHTML = `<span>Size:</span> ${temple.area} sq ft`;
-    card.className = 'imgBox';
-    img.setAttribute("src", temple.imageUrl);
-    img.setAttribute("alt", `${temple.templeName} Temple`);
-    img.setAttribute("loading", "lazy");
+  // Loop through the array and create "temple cards" for each temple by displaying
+  // name, location, date,  total area, image, native lazy loading 
 
-    card.appendChild(name);
-    card.appendChild(location);
-    card.appendChild(dedication);
-    card.appendChild(area);
-    card.appendChild(img);
-    document.querySelector(".home").appendChild(card);
+  document.querySelector(".home").innerHTML = "<h2>Home | All Temples</h2>";
+  createTempleCards(temples);
+
+/* let oldTemples = temples => {
+    return temples.filter(temple => {
+        const year = parseInt(temple.dedicated.split(",")[0], 10);
+        return year < 2000;
+    });
+}; */
+const homeLink = document.getElementById("all temples");
+const oldLink = document.getElementById("old temples");
+const newLink = document.getElementById("new temples");
+const largeLink = document.getElementById("large temples");
+const smallLink = document.getElementById("small temples");
+
+/* EventListener to Home Button */
+homeLink.addEventListener('click', () => {
+
+  document.querySelector(".home").innerHTML = "<h2>Home | All Temples</h2>";
+  createTempleCards(temples);
+})
+
+
+/* EventListener to Old Button */
+oldLink.addEventListener("click", () => {
+  let oldTemples = temples.filter(temple => {
+      const year = parseInt(temple.dedicated.split(",")[0], 10);
+      return year < 1900;
   });
+
+  document.querySelector(".home").innerHTML = "<h2>Old Temples</h2>";
+  createTempleCards(oldTemples);
+  /* console.log(oldTemples); */
 });
+
+
+/* EventListener to New Button */
+newLink.addEventListener("click", () => {
+  let newTemples = temples.filter(temple => {
+      const year = parseInt(temple.dedicated.split(",")[0], 10);
+      return year > 2000;
+  });
+
+  document.querySelector(".home").innerHTML = "<h2>New Temples</h2>";
+  createTempleCards(newTemples);
+/*     console.log(newTemples);
+*/  });
+
+
+/* EventListener to Large Button */
+largeLink.addEventListener("click", () => {
+  let largeTemples = temples.filter(temple => temple.area > 90000);
+  
+  document.querySelector(".home").innerHTML = "<h2>Large Temples</h2>";
+  createTempleCards(largeTemples);
+/*     console.log(largeTemples);
+*/  });
+
+
+/* EventListener to Large Button */
+smallLink.addEventListener("click", () => {
+  let smallTemples = temples.filter(temple => temple.area < 10000);
+  
+  document.querySelector(".home").innerHTML = "<h2>Small Temples</h2>";
+  createTempleCards(smallTemples);
+  console.log(smallTemples);
+});
+
+
+function createTempleCards(templearray) {
+  
+
+  templearray.forEach(temple => {
+      let card = document.createElement("section");
+      let name = document.createElement("h3");
+      let location = document.createElement("p");
+      let dedication = document.createElement("p");
+      let area = document.createElement("p");
+      let img = document.createElement("img");
+
+      name.textContent = temple.templeName;
+      location.innerHTML = `<span class="infoLabel">Location:</span> ${temple.location}`;
+      dedication.innerHTML = `<span>Dedicated:</span> ${temple.dedicated}`;
+      area.innerHTML = `<span>Size:</span> ${temple.area} sq ft`;
+      card.className = 'imgBox';
+      img.setAttribute("src", temple.imageUrl);
+      img.setAttribute("alt", `${temple.templeName} Temple`);
+      // img.setAttribute("loading", "lazy");
+
+      card.appendChild(name);
+      card.appendChild(location);
+      card.appendChild(dedication);
+      card.appendChild(area);
+      card.appendChild(img);
+
+      document.querySelector(".home").appendChild(card);
+
+  });
+  };
