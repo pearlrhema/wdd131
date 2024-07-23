511c627cbef9ae2a7b090a04060fb06798f4b1e1
pearlrhema <pearlrhema8@gmail.com>
2024-07-23 09:10:45 +0100

    utensils.js commited

--- scripts/utensil.js
+++ scripts/utensil.js
@@ -1,27 +1,8 @@
-const date = new Date().getFullYear();
-const dateElement = document.querySelector("#year");
-
-let lastModified = new Date(document.lastModified);
-const lastModifiedElement = document.querySelector("#lastmodified");
-
-dateElement.innerHTML = date;
-lastModifiedElement.innerHTML = lastModified;
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
- 
-
-// Declare and initialize variables
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
 const templesElement = document.getElementById("Delicacies");
-const foodList = [];
+let foodList = [];
 
 /* async displayTemples Function */
 const displayFoodList = (foods) => {
@@ -35,35 +16,40 @@ const displayFoodList = (foods) => {
         img.src = food.imageUrl;
         img.setAttribute('alt', `${food.productName}`);
         myArticle.appendChild(h4);
-        myArticle.appendChild(h3);
         myArticle.appendChild(img);
+        myArticle.appendChild(h3);
         templesElement.appendChild(myArticle);
     };
 }
 
 
 /* async getTemples Function using fetch()*/
-
-displayFoodList(foodList);
+const getFoodListFromJsonFile = async () => {
+    const myresponse = await fetch("https://pearlrhema.github.io/cse121b/utensils.json");
+    let foodData = await myresponse.json();
+    foodList = foodData;
+    displayFoodList(foodList);
+    // console.log(foodList);
+}
 
 /* reset Function */
 const reset = () => {
     templesElement.textContent = "";
-};
+}
 
 /* sortBy Function */
 const sortCategories = (Delicacies) => {
     reset();
     const filter = document.getElementById("sortBy").value;
     switch (filter) {
-        case "stove":
-            displayFoodList(Delicacies.filter(food => food.category.includes("stove")));
-            break;
         case "cylinder":
             displayFoodList(Delicacies.filter(food => food.category.includes("cylinder")));
             break;
         case "burner":
             displayFoodList(Delicacies.filter(food => food.category.includes("burner")));
+            break;
+        case "stove":
+            displayFoodList(Delicacies.filter(food => food.category.includes("stove")));
             break;
         default:
             displayFoodList(foodList);
@@ -72,8 +58,8 @@ const sortCategories = (Delicacies) => {
 }
 
 
-// getFoodListFromJsonFile();
-//reset();
+getFoodListFromJsonFile();
+reset();
 
 /* Event Listener */
 document.getElementById("sortBy").addEventListener('change', () => {sortCategories(foodList)});
